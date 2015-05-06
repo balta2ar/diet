2200ddf1a045295955560a48f3aff5fcc45b7ae1
Yuri Bochkarev <baltazar.bz@gmail.com>
2015-05-06 20:58:32 +0300

    print total % of fat/prots/carbs

--- calories.py
+++ calories.py
@@ -2,10 +2,15 @@ import sys
 import pandas as pd
 
 
-COLUMNS = ['mass', 'fat', 'prot', 'carb', 'calories']
+#: All known columns
+COLUMNS = ['mass', 'fat', 'prot', 'carb', 'cal']
+
+#: Columns that are used to calculate total_% column
+TOTAL = ['fat', 'prot', 'carb']
 
 
 def main(reference_name, values_name):
+    pd.set_option('precision', 2)
     for values_name in values_names:
         print_calories(reference_name, values_name)
 
@@ -26,8 +31,22 @@ def print_calories(reference_name, values_name):
         row['mass'] = values.loc[name].mass
         result = result.append(row)
 
+    # Reorder columns
     result = result[COLUMNS]
-    result = result.append(pd.DataFrame(result.sum(axis=0), columns=['total']).T)
+
+    # Produce total
+    total = pd.DataFrame(result.sum(axis=0), columns=['total']).T
+    result = result.append(total)
+
+    # Produce % of fat, prot, carb
+    total_sum = total[TOTAL].values.sum()
+    total_percent = total * 0
+    total_percent.update(100 * total[TOTAL] / total_sum)
+    total_percent.rename(index={'total': 'total_%'}, inplace=True)
+    result = result.append(total_percent)
+
+    # TODO: add cost/price
+
     print(result)
 
 
